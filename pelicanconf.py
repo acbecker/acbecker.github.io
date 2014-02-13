6802caa5711aec34d8bc7ab7329eb9c9ff53bb8d
Andy Becker <acbecker@gmail.com>
2014-02-12 18:59:22 -0800

    Adding multiple additional headers into pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -98,7 +98,9 @@ if not os.path.exists('_nb_header.html'):
     warnings.warn("_nb_header.html not found. "
                   "Rerun make html to finalize build.")
 else:
-    EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+    EXTRA_HEADER_1 = open('_nb_header.html').read().decode('utf-8')
+    EXTRA_HEADER_2 = open('_table_header.html').read().decode('utf-8')
+    EXTRA_HEADER = EXTRA_HEADER_1 + EXTRA_HEADER_2
 
 # Trying to get the Category panel off the RHS, no effect.
 CATEGORY_SAVE_AS = False
